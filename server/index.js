1a027a21ba6fba29219d7e5e5a793b90cbf5dfba
mubarak23 <mubarakaminu340@gmail.com>
2022-10-07 15:50:08 +0100

    connect to mongodb local

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const { App } = require('./middleware')
 
 const server = express()
+
+
+
+const start = async () => {
+    try{
+     await mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0')
+     console.log('connection to Mongodb was successful')
+    }catch(e){
+     console.log(e)
+    }
+} 
+
+
+
+start()
 
 App(server)
 
