092120306226be8b8faf671ffcd07110bd16bd16
mubarak23 <mubarakaminu340@gmail.com>
2022-10-16 19:26:39 +0100

    order service for backend api calls

---
+++ frontend/src/services/order.js
@@ -0,0 +1,44 @@
+import BaseService from "./base"
+import { ORDER_URL  } from "../constants"
+
+
+class myOrderService extends BaseService {
+    constructor(){
+        super()
+        this.ORDER_URL = ORDER_URL
+    }
+
+    async getOrders(filters) {
+        let requestBody = {}
+        return this.loadData(`${this.ORDER_URL}?${filters}`, requestBody, "GET")
+    }
+
+    async getOrder(id){
+        let requestBody = {}
+        return this.loadData(`${this.ORDER_URL}/${id}`, requestBody, "GET")
+    }
+
+    async addOrder(data){
+        let requestBody = data
+        return this.loadData(this.ORDER_URL, requestBody, "POST")
+    }
+
+    async updateOrderStatus(id, status){
+        let requestBody = {mode: status}
+        return this.loadData(this.ORDER_URL, requestBody, "PUT")
+    }
+
+
+    async editOrderDetails (id, data){
+        let requestBody = data;
+        return this.loadData(`${this.ORDER_URL}/${id}`, requestBody, "PUT")
+    }
+    
+    async deleteOrder (id){
+        let requestBody = {}
+        return this.loadData(`${this.ORDER_URL}/${id}`, requestBody, "DELETE")
+    }
+
+}
+
+export default myOrderService
